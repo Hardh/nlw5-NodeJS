5015f17780c5d5a2b73d74eaaefaad282278b8cd
Alisson Dias <alissondiascangere@hotmail.com>
2021-05-01 19:23:52 -0300

    [master] Aula 2.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes"
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: "Ola NLW 05"
-  });
-})
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usario salvo com sucesso!"
-  })
-
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is runing on port 3333"));
